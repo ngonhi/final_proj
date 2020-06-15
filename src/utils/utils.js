3915ef88a16e70d4d966c1531ec38369f0d25e91
ngonhi <ngonhi@grinnell.edu>
2020-06-16 05:58:23 +0700

    only saves access token to local storage

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,18 +1,19 @@
 export const saveToLocalStorage = (state) => {
-    try {
-        const serializedState = JSON.stringify(state);
-        localStorage.setItem('state', serializedState);
-    } catch (error) {
-        console.log(error);
-    }
-  };
-  
+  try {
+      // Only saves access_token
+      const localState = JSON.stringify({categories: {}, access_token: state['access_token'], error: []})
+      localStorage.setItem('state', localState);
+  } catch (error) {
+      console.log(error);
+  }
+};
+
 export const loadFromLocalStorage = (state) => {
-    try {
-        const serializedState = localStorage.getItem('state');
-        if (serializedState == null) return undefined;
-        return JSON.parse(serializedState);
-    } catch (error) {
-        return undefined;
-    }
-  };
+  try {
+      const serializedState = localStorage.getItem('state');
+      if (serializedState == null) return undefined;
+      return JSON.parse(serializedState);
+  } catch (error) {
+      return undefined;
+  }
+};
